2e679951ee76cf53cec924482954e137ff5285c6
Hlynur Stefánsson <hlynurs15@ru.is>
2016-12-11 18:54:49 +0000

    Implement a little bit better sorting for movies

--- adlo.py
+++ adlo.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from pathlib import PurePath
 import argparse
 import shutil
+import re
+
 
 def main():
     parser = argparse.ArgumentParser(description='Organize a folder containing TV shows')
@@ -12,39 +14,64 @@ def main():
                        help='Path to the directory you want your sorted files to be relocated')
     args = parser.parse_args()
 
-    oldDir = create_path(args.dlFolder)
-    newDir = create_path(args.destFolder)
+    download_folder = create_path(args.dlFolder)
+    destination_folder = create_path(args.destFolder)
 
-    if not oldDir.exists():
+    if not download_folder.exists():
         exit("The given download directory does not exists")
-    if not newDir.exists():
-        create_folders_in_path(newDir)
+    if not destination_folder.exists():
+        create_folders_in_path(destination_folder)
 
-def adlo():
+    adlo(download_folder, destination_folder)
 
-    guessit_keys = ['title', 'season', 'episode', 'episode_title', 'type']
 
-    # Path is hardcoded to begin with
-    working_dir = Path().resolve() / 'downloads'
-    print(working_dir)
+def adlo(download_folder, destination_folder):
+
+    guessit_keys = ['type', 'title', 'season', 'episode', 'episode_title']
+    allowed_formats = ['avi', 'srt', 'mkv', 'mp4', 'mpg', 'mov', 'mpeg', 'flv']
 
     # Create Movies and Episodes folder one folder up from working directory
-    create_folders_in_path(working_dir / '../Movies')
-    create_folders_in_path(working_dir / '../Episodes')
+    movies_folder = destination_folder / 'Movies'
+    episodes_folder = destination_folder / 'Episodes'
+
+    create_folders_in_path(movies_folder)
+    create_folders_in_path(episodes_folder)
 
     # Get list for all files under working directory
-    all_files = [x for x in list(working_dir.glob('**/*')) if x.is_file()]
+    all_files = [x for x in list(download_folder.glob('**/*')) if x.is_file()]
 
-    #print(len(all_files))
-    # Testing function:
-    #create_folders_in_path(working_dir / '../Episodes/South Park/Season 4')
-"""
-    for file in all_files:
-        info = guessit(file.name)
-        if 'title' in info.keys():
-            print(info['title'])
+    for f in all_files:
+        info = guessit(fix_filename(f.name))
+        if 'type' in info.keys():
+            if info['type'].lower() == 'movie':
+                handle_movie(info)
+            elif info['type'].lower() == 'episode':
+                handle_episode(info)
+
+
+def handle_movie(info):
+    """ Sort movie."""
+    # TODO: Cross reference IMDB for title/alternative_title
+    # TODO: Return unprocessed paths (not found in IMDB)
+
+    if 'container' in info.keys() and info['container'].lower() in allowed_formats:
+        if 'title' in info.keys() and 'alternative_title' in info.keys():
+            copy_file(f, movies_folder / (info['title'] + " " + info['alternative_title']))
+        elif 'title' in info.keys():
+            copy_file(f, movies_folder / info['title'])
         else:
-            print('NO TITLE, MOVE ALONG NOTHING TO SEE HERE')"""
+            print("don't know what the title is!")
+            #copy_file(f, movies_folder / f.parent.parts[-1])
+
+
+def handle_episode(info):
+    """ Sort episode."""
+    # do stuff
+
+
+def fix_filename(fname):
+    """ Return filename that can be read by guessit."""
+    return re.sub('\(|\)|\[|\]|{|}',  '', fname)
 
 
 def copy_file(f, target_path):
@@ -61,7 +88,9 @@ def create_folders_in_path(path):
         path.mkdir()
     return
 
+
 def create_path(path):
+    """ Returns correct Path object regardless of absolute or relative path."""
         if PurePath(path).is_absolute():
             return Path(path)
         else:
