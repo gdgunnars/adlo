2b94a8a768d5c2e220d7eece19bee3e97163dba9
GD <gdg@gdg.is>
2016-12-11 17:47:10 +0000

    Added main function and a validation that given paths are correct. Created a helper funciton called create_path() to help with path recognition

--- adlo.py
+++ adlo.py
@@ -1,10 +1,27 @@
 from guessit import guessit
 from pathlib import Path
+from pathlib import PurePath
+import argparse
 import shutil
 
+def main():
+    parser = argparse.ArgumentParser(description='Organize a folder containing TV shows')
+    parser.add_argument('dlFolder',
+                       help='Path to download folder')
+    parser.add_argument('destFolder',
+                       help='Path to the directory you want your sorted files to be relocated')
+    args = parser.parse_args()
+
+    oldDir = create_path(args.dlFolder)
+    newDir = create_path(args.destFolder)
+
+    if not oldDir.exists():
+        exit("The given download directory does not exists")
+    if not newDir.exists():
+        create_folders_in_path(newDir)
 
 def adlo():
-    
+
     guessit_keys = ['title', 'season', 'episode', 'episode_title', 'type']
 
     # Path is hardcoded to begin with
@@ -14,7 +31,7 @@ def adlo():
     # Create Movies and Episodes folder one folder up from working directory
     create_folders_in_path(working_dir / '../Movies')
     create_folders_in_path(working_dir / '../Episodes')
-    
+
     # Get list for all files under working directory
     all_files = [x for x in list(working_dir.glob('**/*')) if x.is_file()]
 
@@ -30,11 +47,11 @@ def adlo():
             print('NO TITLE, MOVE ALONG NOTHING TO SEE HERE')"""
 
 
-def copy_file(file, target_path):
+def copy_file(f, target_path):
     """ Create target_path if it doesn't exist and copy file to target_path."""
     if not target_path.exists():
         create_folders_in_path(target_path)
-    shutil.copy(str(from_path), str(target_path))
+    shutil.copy(str(f), str(target_path))
 
 
 def create_folders_in_path(path):
@@ -43,3 +60,12 @@ def create_folders_in_path(path):
         create_folders_in_path(path.parent)
         path.mkdir()
     return
+
+def create_path(path):
+        if PurePath(path).is_absolute():
+            return Path(path)
+        else:
+            return Path().resolve() / path
+
+if __name__ == "__main__":
+    main()
